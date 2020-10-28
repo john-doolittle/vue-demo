d36a106fd7a8cac1178dd285d17fb5ba6eba19b3
John Doolittle <jdoo.code@gmail.com>
2020-10-28 17:51:33 -0600

    Creater counter button component

---
+++ my-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Yo watup</h3>
+    <CounterBtn/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CounterBtn from './components/CounterBtn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CounterBtn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
